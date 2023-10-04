16579b025fca6119d315e39f45b48557ee481edb
aikkel <mikk39k8@edu.zealand.dk>
2023-10-04 12:56:35 +0200

    added remove button

--- script.js
+++ script.js
@@ -2,21 +2,42 @@
 const taskInput = document.getElementById('task');
 const addTaskButton = document.getElementById('addTask');
 const taskList = document.getElementById('taskList');
-const removeTaskButton = document.getElementById('removeTask')
+
+// Function to create a new task item with a "Remove" button
+function createTaskItem(text) {
+    const taskItem = document.createElement('li');
+    
+    const taskText = document.createElement('span');
+    taskText.textContent = text;
+    taskItem.appendChild(taskText);
+
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'Remove';
+    removeButton.classList.add('remove-button');
+
+    // Event listener to remove the task when the button is clicked
+    removeButton.addEventListener('click', () => {
+        taskItem.remove();
+    });
+
+    taskItem.appendChild(removeButton);
+    return taskItem;
+}
+
 // Event listener for adding tasks
 addTaskButton.addEventListener('click', () => {
     const taskText = taskInput.value.trim();
     if (taskText !== '') {
-        const taskItem = document.createElement('li');
-        taskItem.textContent = taskText;
+        const taskItem = createTaskItem(taskText);
         taskList.appendChild(taskItem);
         taskInput.value = ''; // Clear the input field
     }
 });
 
-// Event listener for removing tasks
+// Event listener for removing tasks (using event delegation)
 taskList.addEventListener('click', (e) => {
-    if (e.target.tagName === 'LI') {
-        e.target.remove();
+    if (e.target.classList.contains('remove-button')) {
+        const taskItem = e.target.parentElement;
+        taskItem.remove();
     }
 });
